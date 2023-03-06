3667233f9e767e0eaa76d0eed7c8a773a4707f82
qiyuanchen <chenqiyuan1012@foxmail.com>
2023-03-06 19:27:13 +0800

    init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    filename = file.filename
+    return jsonify({'filename': filename})
+
+if __name__ == '__main__':
+    app.run(debug=True)
